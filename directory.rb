e1bf69b367158a4a134b1bf5051963520fdce81f
Natalie Theodoropoulou <natalietheo@mastude5grpq6l4.home>
2022-11-07 20:44:33 +0000

    adding hashes and cohorts, making code more readable

--- directory.rb
+++ directory.rb
@@ -1,30 +1,32 @@
-students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex DeLarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Joffrey Baratheon",
-"Norman Bates",
-]
+def input_students 
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    students = []
+    name = gets.chomp
+    while !name.empty? do
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        name = gets.chomp
+    end
+    students
+end
 def print_header 
     puts "The students of Villains Academy"
     puts "----------"
 end 
-def print(names)
-    names.each do |name|
-        puts name
+def print(students)
+    students.each do |student|
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
 end
 
-def print_footer(names)
+def print_footer(students)
     puts "Overall, we have #{students.count} great students"
 end
 
+
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
